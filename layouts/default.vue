ab54d4a0fdfa98039f5f197b99827c16bb913ce9
Joe Ihata <joeihata@JoenoMacBook-Air.local>
2019-12-31 23:59:07 +0900

    30までの内容を加味できた

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    <Header />
     <nuxt />
   </div>
 </template>
 
-<script>
-import Header from '@/components/Header'
-
-export default {
-  components: {
-    Header
-  }
-}
-</script>
-
 <style>
+html {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
 
 </style>
